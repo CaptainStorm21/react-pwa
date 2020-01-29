00298a440f78d7eb6af1430c6d4ecc0a63bd0fd8
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-01-28 23:17:46 -0800

    pwa installed
    please  install 
    npm i -g serve
    this will build the pwa
    serve -s build

---
+++ build/precache-manifest.9c2da0f34b618ad3166c66cee2ea1a44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25f83af374ec6a7f304fd70a853984fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6f829d26017530eaf85",
+    "url": "/static/css/main.38283f52.chunk.css"
+  },
+  {
+    "revision": "f809e39233cd658e47bf",
+    "url": "/static/js/2.024e2663.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.024e2663.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a6f829d26017530eaf85",
+    "url": "/static/js/main.f888ed8b.chunk.js"
+  },
+  {
+    "revision": "7dafbefeb6761ab3bd40",
+    "url": "/static/js/runtime-main.5a742515.js"
+  }
+]);
