c607325bd64e8cb74a3ec53200aecc85b0243e1d
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-01-28 22:49:17 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect} from 'react';
+import './App.css';
+
+function App() {
+  const [data, setData] =useState ([])
+
+  //data fetching
+  useEffect (() => {
+    const fetchData = async() =>{
+      const result = await fetch ("http://orangevalleycaa.org/api/videos").then (
+        response => response.json()
+      )
+      setData(result);
+    }
+    fetchData();
+  });
+  return (
+    <div className="App">
+      <header>
+        <h1> Videos </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
